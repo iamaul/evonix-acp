c202972d9e0cc2a4ab971f4b2c5691da33ccabd9
Agung Maulana <iamaul@hotmail.com>
2020-05-15 10:28:13 +0700

    updated latest changes

--- src/components/pages/userapps/UserApps.jsx
+++ src/components/pages/userapps/UserApps.jsx
@@ -1,22 +1,28 @@
-import React, { useEffect, useContext, useMemo, useState } from 'react';
+import React, { useEffect, useContext, useMemo, useCallback } from 'react';
 import DataTable from 'react-data-table-component';
-import moment from 'moment';
+import Moment from 'react-moment';
 import { Modal, Button, Image, Header, Divider, Label } from 'semantic-ui-react';
 
 import UserAppsContext from '../../../context/userapps/userAppsContext';
-
-import ActionButton from './ActionButton';
 
 import Loader from '../../layouts/loader/Loader';
 
 const UserApps = () => {
     const userAppsContext = useContext(UserAppsContext);
-    const { user_apps, getAllUserApps, setLoading } = userAppsContext;
+    const { user_apps, getAllUserApps, updateUserApps, setLoading } = userAppsContext;
 
     useEffect(() => {
         getAllUserApps();
         // eslint-disable-next-line
     },[])
+
+    const onUserApprove = useCallback((id, user_id) => {
+        updateUserApps(1, id, user_id);
+    }, []);
+
+    const onUserDeny = useCallback((id, user_id) => {
+        updateUserApps(1, id, user_id);
+    }, []);
 
     const columns = useMemo(() => [
         {
@@ -65,22 +71,33 @@ const UserApps = () => {
             name: 'Created at',
             selector: 'created_at',
             sortable: true,
-            format: row => moment(row.created_at).format('lll')
+            cell: row => <div><Moment unix format="llll">{row.created_at}</Moment></div>
         },
         {
             name: 'Updated at',
             selector: 'updated_at',
             sortable: true,
-            format: row => moment(row.created_at).format('lll')
+            cell: row => <div>{row.updated_at !== null ? (<Moment unix format="llll">{row.created_at}</Moment>) : 'No update'}</div>
         },
         {
-            name: "Action",
-            cell: row => (
-                <ActionButton
-                    id={row.id}
-                    user_id={row.user_id}
-                    status={row.userAppUser && row.userAppUser.status}
-                />
+            name: 'Action',
+            sortable: false,
+            button: true,
+            cell: (row) => (
+                <Button.Group size="small">
+                    {row.userAppUser && row.userAppUser.status === 1 && (<div>
+                        <Button
+                            icon="checkmark"
+                            color="green"
+                            onClick={() => onUserApprove(row.id, row.user_id)}
+                        />
+                        <Button
+                            icon="delete"
+                            color="red"
+                            onClick={() => onUserDeny(row.id, row.user_id)}
+                        />
+                    </div>)}
+                </Button.Group> 
             )
         }
     ], []);
