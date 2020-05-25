0530d3a2dca0e4a5b4025025c93b0151dddc1b27
Agung Maulana <iamaul@hotmail.com>
2020-05-25 12:18:55 +0700

    updated latest changes

--- src/components/pages/news/NewsForm.jsx
+++ src/components/pages/news/NewsForm.jsx
@@ -92,7 +92,7 @@ const NewsForm = () => {
                                 'searchreplace visualblocks code fullscreen',
                                 'insertdatetime media table paste code wordcount'
                             ],
-                            toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl'
+                            toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen preview save print | insertfile image media template link anchor codesample | ltr rtl'
                         }}
                         onEditorChange={onEditorChange}
                     />
