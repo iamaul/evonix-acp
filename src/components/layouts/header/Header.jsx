54ac81efa3d06246700b55e20998b5c44a0c08a7
Agung Maulana <iamaul@hotmail.com>
2020-06-14 02:56:38 +0700

    updated latest changes

--- src/components/layouts/header/Header.jsx
+++ src/components/layouts/header/Header.jsx
@@ -10,7 +10,8 @@ const Header = () => {
                 <Image centered src="/assets/images/evonix-logo.png" size="massive"/><br/>
                 Admin Panel
                 <Head.Subheader>
-                    To prevent hacking into your account, please make sure to log out of your account once you have done with your session.
+                    To prevent hacking into your account, please make sure to log out of your account once you have done with your session.<br/>
+                    <b>Note</b>: Kalau ada <i>bug</i> di UCP, suruh <i>submit</i> ke <a href="https://support.evonix-rp.com">sini</a> ya!
                 </Head.Subheader><br/>
             </Head>
             <Navbar />
