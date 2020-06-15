9f28799380b3c980dd3b613ed592cf9238161446
Agung Maulana <iamaul@hotmail.com>
2020-06-16 01:44:35 +0700

    updated api base url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({ 
-    baseURL: 'https://server.evonix-rp.com/', 
+    baseURL: 'https://dev.evonix-rp.com/', 
     headers: {
         'Content-Type': 'application/json'
     }
