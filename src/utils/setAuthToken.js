dbb0d305f54c5fc20da73c75d7570888a5ded4d5
Agung Maulana <iamaul@hotmail.com>
2020-06-17 21:00:19 +0700

    updated setAuthToken

--- src/utils/setAuthToken.js
+++ src/utils/setAuthToken.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import api from './api';
 
 const setAuthToken = token => {
     if (token) {
-        axios.defaults.headers.common['x-auth-token'] = token;
+        api.defaults.headers.common['x-auth-token'] = token;
     } else {
-        delete axios.defaults.headers.common['x-auth-token'];
+        delete api.defaults.headers.common['x-auth-token'];
     }
 }
 
