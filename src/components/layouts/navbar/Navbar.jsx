838b94e8e1da7b5fc001bfe668556297e24b7211
Agung Maulana <iamaul@hotmail.com>
2020-06-17 11:59:21 +0700

    fixed undefined useContext

--- src/components/layouts/navbar/Navbar.jsx
+++ src/components/layouts/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Menu, Dropdown, Icon } from 'semantic-ui-react';
 import AuthContext from '../../../context/auth/authContext';
