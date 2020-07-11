90963f0c7324a1bc6c2e9a79c9b7f17fc629da3b
Agung Maulana <iamaul@hotmail.com>
2020-07-11 13:53:52 +0700

    updated latest changes

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,9 +3,7 @@ import api from './api';
 export const setAuthToken = token => {
     if (token) {
         api.defaults.headers.common['x-auth-token'] = token;
-        localStorage.setItem('token', token);
     } else {
         delete api.defaults.headers.common['x-auth-token'];
-        localStorage.removeItem('token');
     }
 }
